25a68419139c40db9ff79d1cf96f632d1faec41a
wilsonkao <wilkao036@gmail.com>
2021-08-18 00:30:31 +0800

    add error boundary

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCheck, faPlus } from "@fortawesome/free-solid-svg-icons";
-import Layout from "./components/common/Layout";
+import Layout from "components/common/Layout";
 import movieInfo from "pages/MovieInfoPage";
 import singUp from "pages/SingUpPage";
 import singIn from "pages/SingInPage";
@@ -12,6 +12,8 @@ import forgetPassword from "pages/ForgetPasswordPage";
 import resetPassword from "pages/ResetPasswordPage";
 import myList from "pages/MyListPage";
 import PrivateRoute from "components/common/PrivateRoute";
+import NotFoundPage from "pages/NotFoundPage";
+import ErrorBoundary from "components/ErrorBoundary";
 
 library.add(fab, faCheck, faPlus);
 
@@ -19,15 +21,18 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Layout>
-        <Switch>
-          <Route path="/" exact component={home} />
-          <Route path="/singUp" component={singUp} />
-          <Route path="/singIn" component={singIn} />
-          <Route path="/forgetPassword" component={forgetPassword} />
-          <Route path="/change-password/:token" component={resetPassword} />
-          <Route path="/movie-info/:movieId" component={movieInfo} />
-          <PrivateRoute path="/myList" component={myList} />
-        </Switch>
+        <ErrorBoundary>
+          <Switch>
+            <Route path="/" exact component={home} />
+            <Route path="/singUp" component={singUp} />
+            <Route path="/singIn" component={singIn} />
+            <Route path="/forgetPassword" component={forgetPassword} />
+            <Route path="/change-password/:token" component={resetPassword} />
+            <Route path="/movie-info/:movieId" component={movieInfo} />
+            <Route component={NotFoundPage} />
+            <PrivateRoute path="/myList" component={myList} />
+          </Switch>
+        </ErrorBoundary>
       </Layout>
     </Router>
   );
